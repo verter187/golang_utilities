3fb303168efdaeece4a39a7d5cc9c30dd8b16b77
verter187 <verter187@inbox.ru>
2023-04-01 16:31:35 +0300

    new commit_20230401

--- app_golang.go
+++ app_golang.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"strings"
 )
 
@@ -16,36 +15,30 @@ func Dedupe[T comparable](arr []T) []T {
 	return uniq
 }
 
-const refString = "test" //"Вася Петя, Вася! Леня Петя Женя Игорь"
-
-func test() {
-	words := strings.Fields(refString)
-	for idx, word := range words {
-		fmt.Printf("Word %d is: %s\n", idx, word)
-	}
-	fmt.Println(Dedupe(words))
-}
-
 func main() {
-	fmt.Println(Dedupe([]string{"a", "b", "a", "c"})) // [a b c]
-	test()
 
 	// для более мелких файлов
-	fContent, err := ioutil.ReadFile("./temp/anna1.txt")
-	if err != nil {
-		panic(err)
-	}
+	// filename := os.Args[1] //"./temp/anna1.txt"
+	// fContent, err := ioutil.ReadFile(filename)
+	// if err != nil {
+	// 	panic(err)
+	// }
 
 	replace_marks_str := "!,.?:;\"-[]()123456789"
 	replace_marks := strings.SplitAfter(replace_marks_str, "")
-	content := string(fContent)
-	for i := 0; i < len(replace_marks); i++ {
-		content = strings.ReplaceAll(content, replace_marks[i], "")
+	// content := string(fContent)
+
+	// for i := 0; i < len(replace_marks); i++ {
+	// 	content = strings.ReplaceAll(content, replace_marks[i], "")
+	// }
+	for i := range replace_marks {
+		// content = strings.ReplaceAll(content, mark, "")
+		fmt.Printf(i, "\n")
 	}
 
-	words := strings.Fields(content)
+	// words := strings.Fields(content)
 
-	fmt.Println(len(words))
-	fmt.Println(len(Dedupe(words)))
+	// fmt.Println(len(words))
+	// fmt.Println(len(Dedupe(words)))
 
 }
